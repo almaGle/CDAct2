52f7c5dc2baf9c834603c3259026dde8e5c3367e
almaGle <almag081101@gmail.com>
2023-10-06 20:44:50 -0600

    new commit

---
+++ bestCase.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int a[6]={5,7,6,-1,2,0};
+    for (int j=1; j<=6; j++){
+        int key=a[j];
+        int i=j-1;
+        while (i>=0 && a[i]>key){
+            a[i+1]=a[i];
+            i=i-1;
+            a[i+1]=key;
+            
+        }
+        
+    }
+    for (int m=0; m<6;m++){
+        cout<<a[m];
+    }
+    
+}
